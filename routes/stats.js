58b8602a3a656287e172472acbf9e1ec75725463
Mushra <mushra@Mushras-MacBook-Pro.local>
2019-06-22 23:41:53 +0200

    redundant stats, cool hover effect

--- routes/stats.js
+++ routes/stats.js
@@ -43,11 +43,39 @@ router.route('/stats/').all(async (req, res) => {
     return b.intensity - a.intensity
   })
 
-  console.log(linksFired)
+  // tags, ordered by how many pages link to them (how diluted they are)
+  let tagsByDilution = []
+  allIncoming.forEach(tag => {
+    let sources = 0
+    global.complete.forEach(page => {
+      if (page.outgoing.includes(tag)) sources += 1
+    })
+    let real = allOutgoing.includes(tag)
+    tagsByDilution.push({ tag, sources, real })
+  })
+  tagsByDilution.sort((a, b) => {
+    return b.sources - a.sources
+  })
+
+  // links, ordered by how many pages they lead to (how many targets they have)
+  let linksByTarget = []
+  allOutgoing.forEach(link => {
+    let targets = 0
+    global.complete.forEach(page => {
+      if (page.incoming.includes(link)) targets += 1
+    })
+    let real = allIncoming.includes(link)
+    linksByTarget.push({ link, targets, real })
+  })
+  linksByTarget.sort((a, b) => {
+    return b.targets - a.targets
+  })
 
   let rendertime = '' + global.complete.length + ' pages~'
+  let bazinga = { allLinks: global.allLinks }
+  bazinga = JSON.stringify(bazinga)
   if (req.query.time) rendertime += ' took ' + req.query.time + ' mseccs to build'
-  res.render('stats.handlebars', { tagReaches, linksFired, rendertime })
+  res.render('stats.handlebars', { tagReaches, linksFired, tagsByDilution, linksByTarget, rendertime, bazinga })
 })
 
 module.exports = router
---
+++ static/pagesOnHover.js
@@ -0,0 +1,62 @@
+// i'm gonna add event listeners to like 5000 nodes
+let width = document.documentElement.clientWidth
+let height = document.documentElement.clientHeight
+let body = document.querySelector('body')
+let li = document.querySelectorAll('li.page')
+let allLinks = bazinga.allLinks // bazinga
+console.log(allLinks)
+let boxes = []
+li.forEach(link => {
+  link.addEventListener('mouseenter', event => {
+    let box = createBox(link)
+    let boxSize = boxdims(box, event.clientX, event.clientY)
+    box.style.left = boxSize.x + 'px'
+    box.style.top = boxSize.y + 'px'
+    body.appendChild(box)
+    boxes.push(box)
+  })
+  link.addEventListener('mousemove', event => {
+    if (boxes.length > 0) {
+      boxes.forEach(box => {
+        let boxSize = boxdims(box, event.clientX, event.clientY)
+        box.style.left = boxSize.x + 'px'
+        box.style.top = boxSize.y + 'px'
+      })
+    }
+  })
+  link.addEventListener('mouseleave', event => {
+    if (boxes.length > 0) {
+      boxes.forEach(box => {
+        body.removeChild(box)
+      })
+      boxes = []
+    }
+  })
+})
+console.log('foreach')
+
+function boxdims (box, mx, my) {
+  let space = 10
+  let x = mx
+  let y = my
+  if (mx + box.offsetWidth > width) x = mx - box.offsetWidth - space
+  if (my + box.offsetHeight > height) y = height - box.offsetHeight
+  return { x, y }
+}
+
+function createBox (link) {
+  let actualLink = link.innerHTML.split(' ')[0].trim()
+  let box = document.createElement('div')
+  if (allLinks[actualLink]) {
+    let ul = document.createElement('div')
+    ul.classList.add('targets')
+    allLinks[actualLink].forEach(target => {
+      let li = document.createElement('span')
+      li.innerText = target
+      ul.appendChild(li)
+    })
+    box.appendChild(ul)
+  }
+  box.classList.add('bawrks')
+  return box
+}
